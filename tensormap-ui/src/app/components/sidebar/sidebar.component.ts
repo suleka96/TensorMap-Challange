985df5353abe9b8b3ef7faee73b26a4b002b64a7
suleka96 <sulekahelmini96comp@gmail.com>
2019-03-23 21:55:30 +0530

    Make initial layout

---
+++ tensormap-ui/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
+
+}
